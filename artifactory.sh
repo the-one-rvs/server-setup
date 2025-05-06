2679847a93ac3c72a8a6cce8d7719c52672ca7fe
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-06 12:36:25 +0530

    Refactor Artifactory installation script to use APT for package management

--- artifactory.sh
+++ artifactory.sh
@@ -1,15 +1,9 @@
 sudo apt update
 sudo apt install openjdk-21-jdk -y
-java -version
 
-#--Jfrog Artifactory---#
-curl -g -L -O -J 'https://releases.jfrog.io/artifactory/artifactory-pro/org/artifactory/pro/jfrog-artifactory-pro/[RELEASE]/jfrog-artifactory-pro-[RELEASE]-linux.tar.gz'
-
-# Extract
-tar -xvf jfrog-artifactory-pro-7.104.10-linux.tar.gz
-
-# Move to /opt
-sudo mv artifactory-oss-7.77.3 /opt/artifactory
-
-cd /opt/artifactory/app/bin
-./artifactory.sh start
+curl -sL https://bintray.com/jfrog/artifactory-rpms/rpm > /etc/apt/sources.list.d/jfrog.list
+sudo apt update
+sudo apt install jfrog-artifactory-oss -y
+sudo systemctl enable artifactory
+sudo systemctl start artifactory
+sudo systemctl status artifactory

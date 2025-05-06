d26916c4c4c9fa1779cea86318d25ef623ceb4fd
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-06 12:44:21 +0530

    new

--- artifactory.sh
+++ artifactory.sh
@@ -1,6 +1,3 @@
-sudo apt update
-sudo apt install openjdk-21-jdk -y
-
 curl -sL https://bintray.com/jfrog/artifactory-rpms/rpm > /etc/apt/sources.list.d/jfrog.list
 sudo apt update
 sudo apt install jfrog-artifactory-oss -y

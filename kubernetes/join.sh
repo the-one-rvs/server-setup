95abf55f233c11e9ee30e756c10c20f8f1a67d8f
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-06 18:30:19 +0530

    join

--- kubernetes/join.sh
+++ kubernetes/join.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# This script joins the worker node to the Kubernetes cluster
+echo "[INFO] Joining Kubernetes cluster..."
+sudo kubeadm join 192.168.86.133:6443 --token gpgq2t.5lqwg7lnzmthalwp \
+--discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74decdc32e6785b850f95a
 
-JOIN_CMD="kubeadm join 192.168.86.133:6443 \
---token gpgq2t.5lqwg7lnzmthalwp \
---discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74dedc32e6785b850f95a"
-
-echo "[INFO] Joining the Kubernetes cluster..."
-sudo $JOIN_CMD

7f7a1f72f92939caaee1bb7502483d2ed24f1d14
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-07 10:11:06 +0530

    ok

--- kubernetes/k9s.sh
+++ kubernetes/k9s.sh
@@ -1,3 +1,4 @@
-wget https://github.com/derailed/k9s/releases/download/v0.27.3/k9s_Linux_x86_64.tar.gz
-tar -zxvf k9s_Linux_x86_64.tar.gz
-sudo mv k9s /usr/local/bin
+VERSION=v0.32.4  # <- replace with latest if needed
+wget https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz
+tar -zxvf k9s_Linux_amd64.tar.gz
+sudo mv k9s /usr/local/bin/

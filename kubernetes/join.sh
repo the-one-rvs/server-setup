525e16bb9303a8dfcbc64805e23830bdf72b0ea8
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-10 19:42:20 +0530

    new join command

--- kubernetes/join.sh
+++ kubernetes/join.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "[INFO] Joining Kubernetes cluster..."
-sudo kubeadm join 192.168.86.133:6443 --token gpgq2t.5lqwg7lnzmthalwp \
---discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74decdc32e6785b850f95a
+kubeadm join 192.168.86.133:6443 --token g8jjah.d6a15m0w8jj56mu13 \
+    --discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74dedcc32e6785b850f95a
 

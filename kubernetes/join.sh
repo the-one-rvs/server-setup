e56521698a29d362f8307238e8f0c76774c850e8
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-10 21:51:40 +0530

    it must run

--- kubernetes/join.sh
+++ kubernetes/join.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "[INFO] Joining Kubernetes cluster..."
-kubeadm join 192.168.86.133:6443 --token g8jjahd.6a15m0w8jj56mu13 \
-    --discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74dedcc32e6785b850f95a
-
+sudo kubeadm join 192.168.86.133:6443 --token dpr81w.lca29w5u0a8dpsur --disco very-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74decdc32e6785b850f95a

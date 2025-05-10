542e325076df8a855718d3cc577e41713f59df9a
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-10 19:50:22 +0530

    fix: correct token format in join script

--- kubernetes/join.sh
+++ kubernetes/join.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "[INFO] Joining Kubernetes cluster..."
-kubeadm join 192.168.86.133:6443 --token g8jjah.d6a15m0w8jj56mu13 \
+kubeadm join 192.168.86.133:6443 --token g8jjahd.6a15m0w8jj56mu13 \
     --discovery-token-ca-cert-hash sha256:5f956ef9b17ef6892d4f2068c13725b68ccc83342d74dedcc32e6785b850f95a
 

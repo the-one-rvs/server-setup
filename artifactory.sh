3de380ecaa9d5162ee9ca094f02e0086dfd0624d
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-13 11:31:54 +0530

    try

--- artifactory.sh
+++ artifactory.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt install openjdk17-jdk
 echo "deb https://releases.jfrog.io/artifactory/artifactory-debs xenial main" | sudo tee -a /etc/apt/sources.list.d/artifactory.list
 curl -fsSL  https://releases.jfrog.io/artifactory/api/gpg/key/public|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/artifactory.gpg
 sudo apt update

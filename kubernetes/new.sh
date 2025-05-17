c48695fb416bee25a2f50c1870d542a984254563
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-17 20:18:43 +0530

    ip-pool

--- kubernetes/new.sh
+++ kubernetes/new.sh
@@ -60,7 +60,7 @@ kubelet --version
 kubectl version --client
 
 sudo crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config

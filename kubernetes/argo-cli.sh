a9a895fccbf44b2ea4c88ae1d05d8c441002edba
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-17 22:01:44 +0530

    ip-pool

--- kubernetes/argo-cli.sh
+++ kubernetes/argo-cli.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 set -e
-
-# Set the version you want to install
-ARGO_VERSION="v3.4.16"
-
-# Download the binary
-curl -sSL -o argo-linux-amd64.gz "https://github.com/argoproj/argo-workflows/releases/download/${ARGO_VERSION}/argo-linux-amd64.gz"
-
-# Unzip
-gunzip argo-linux-amd64.gz
-
-# Make it executable
-chmod +x argo-linux-amd64
-
-# Move to /usr/local/bin
-sudo mv argo-linux-amd64 /usr/local/bin/argo
-
-# Verify installation
-argo version
+# Download and install ArgoCD CLI
+ARGOCD_VERSION="v2.10.6"
+curl -sSL -o argocd-linux-amd64 "https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-amd64"
+chmod +x argocd-linux-amd64
+sudo mv argocd-linux-amd64 /usr/local/bin/argocd
+argocd version --client

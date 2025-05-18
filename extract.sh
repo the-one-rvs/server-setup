43f2cb1cafc54f48f5454ac110bf095c988902e7
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-05-18 18:22:29 +0530

    ip-pool

---
+++ extract.sh
@@ -0,0 +1,7 @@
+SECRET=$(kubectl -n kube-system get sa argocd-manager -o jsonpath="{.secrets[0].name}")
+
+TOKEN=$(kubectl -n kube-system get secret $SECRET -o jsonpath="{.data.token}" | base64 --decode)
+
+CA_CRT=$(kubectl -n kube-system get secret $SECRET -o jsonpath="{.data['ca\.crt']}" | base64 --decode)
+
+API_SERVER=$(kubectl config view --raw -o jsonpath="{.clusters[?(@.name==\"$(kubectl config current-context)\")].cluster.server}")
